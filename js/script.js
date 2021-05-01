286a3a73191407821a99941c98c0b1ec5ab05c0f
Aleksandr Tloka <aleksandr.tloka@i.ua>
2021-05-01 22:02:30 +0300

    fixed adaptive

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
               breakpoint: 1650,
               settings: {
                 dots: false,            
-                prevArrow: '<button type="button" class="slick-prev"><img src="img/Left.svg" alt="prev"></button>',
+                prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg" alt="prev"></button>',
                 nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg" alt="next"></button>'
               }
             }
@@ -40,7 +40,7 @@ $(document).ready(function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/Left_advent.png" alt="prev"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left_advent.png" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/Right_advent.png" alt="next"></button>',
         responsive: [
           {
